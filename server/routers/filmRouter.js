f3e8ce801979abc8b246a0c9101f8882584a0051
hachihao792001 <hachihao792001@gmail.com>
2021-12-16 22:55:47 +0700

    add filter by genre route in filmRouter and function in FilmController

--- server/routers/filmRouter.js
+++ server/routers/filmRouter.js
@@ -1,16 +1,25 @@
-import express from 'express';
+import express from "express";
 
-
-import { isAuth,isAdmin } from '../middleware/auth.js';
-import { createFilm,getFilm,updateFilm,deleteFilm,getAllFilms } from '../controllers/FilmController.js';
-const filmRouter=express.Router();
+import { isAuth, isAdmin } from "../middleware/auth.js";
+import {
+  createFilm,
+  getFilm,
+  updateFilm,
+  deleteFilm,
+  getAllFilms,
+  getFilmByGenre,
+} from "../controllers/FilmController.js";
+const filmRouter = express.Router();
 
 // filmRouter.put('/update',updateAll)
-filmRouter.get('/films',isAuth,getAllFilms);
-filmRouter.post('/',isAuth,isAdmin,createFilm);
-filmRouter.get('/:id',isAuth,getFilm);
-filmRouter.put('/:id',isAuth,isAdmin,updateFilm);
-filmRouter.delete('/:id',isAuth,isAdmin,deleteFilm);
-
+filmRouter.get("/films", isAuth, getAllFilms);
+filmRouter.post("/", isAuth, isAdmin, createFilm);
+filmRouter.get("/:id", isAuth, getFilm);
+filmRouter.put("/:id", isAuth, isAdmin, updateFilm);
+filmRouter.delete("/:id", isAuth, isAdmin, deleteFilm);
+filmRouter.get("/genre/:arg", isAuth, getFilmByGenre);
+//filmRouter.get("/type/:arg", isAuth, getFilmByType);
+//filmRouter.get("/nation/:arg", isAuth, getFilmByNation);
+//filmRouter.get("/year/:arg", isAuth, getFilmByYear);
 
 export default filmRouter;
