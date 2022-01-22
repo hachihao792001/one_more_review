36c1c5d5f34f6ef86fe96f9d075aaa0ef46fa0a9
hachihao792001 <hachihao792001@gmail.com>
2022-01-23 00:31:27 +0700

    format router files

--- server/routers/commentRouter.js
+++ server/routers/commentRouter.js
@@ -1,14 +1,18 @@
-import express from 'express';
+import express from "express";
 
-import {isAuth} from '../middleware/auth.js'
+import { isAuth } from "../middleware/auth.js";
 
-const commentRouter=express.Router();
+const commentRouter = express.Router();
 
-import {createComment,getComment,updateComment,deleteComment} from '../controllers/CommentController.js'
-commentRouter.get('/:id',isAuth,getComment)
-commentRouter.post('/',isAuth,createComment);
-commentRouter.put('/:id',isAuth,updateComment);
-commentRouter.delete('/:id',isAuth,deleteComment);
-
+import {
+  createComment,
+  getComment,
+  updateComment,
+  deleteComment,
+} from "../controllers/CommentController.js";
+commentRouter.get("/:id", isAuth, getComment);
+commentRouter.post("/", isAuth, createComment);
+commentRouter.put("/:id", isAuth, updateComment);
+commentRouter.delete("/:id", isAuth, deleteComment);
 
 export default commentRouter;
